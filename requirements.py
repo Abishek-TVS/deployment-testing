6fd1555935065b1fad654174e8aef66a071ba022
ABI <tvsabishek@gmail.com>
2020-06-26 20:31:21 +0530

    requiremntsfile

---
+++ requirements.py
@@ -0,0 +1,11 @@
+argon2-cffi==20.1.0
+asgiref==3.2.7
+cffi==1.14.0
+numpy==1.18.5
+Pillow==7.1.2
+pycparser==2.20
+python-dateutil==2.8.1
+pytz==2020.1
+six==1.15.0
+sqlparse==0.3.1
+text-unidecode==1.3
